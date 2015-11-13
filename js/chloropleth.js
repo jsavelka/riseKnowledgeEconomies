90878ee680dd38a8a1013c6f2369f1d42f5f0972
jsavelka <j.savelka@gmail.com>
2015-11-13 14:51:01 -0500

    Initial boilerplate for the project

---
+++ js/chloropleth.js
@@ -0,0 +1,6 @@
+var width  = 800,
+    height = 600;
+
+var chart  = d3.select("#chloropleth")
+    .attr("width",  width)
+    .attr("height", height);
---
+++ js/timeLine.js
@@ -0,0 +1,6 @@
+var width  = 800,
+    height = 600;
+
+var chart  = d3.select("#timeLine")
+    .attr("width",  width)
+    .attr("height", height);
